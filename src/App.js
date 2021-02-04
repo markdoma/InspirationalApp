055ed6efcd98528315a62ae34c3cec9411540cc1
unknown <markdoma10@gmail.com>
2021-02-05 06:42:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+import HomePage from './HomePage.component'
+
+function App() {
+  return (
+    <div className="App">
+     <HomePage />
+  
+    </div>
+  );
+}
+
+export default App;
