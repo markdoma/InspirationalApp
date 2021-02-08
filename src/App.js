8a8fffece2c3ad974ecb7f30c7f600cfd2ec1eae
unknown <markdoma10@gmail.com>
2021-02-09 07:54:38 +0800

    adding header component and attach to home the header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/Homepage/Homepage.component";
 import ShopPage from "./Pages/Shop/shop.component";
+import Header from "./Components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
+
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
