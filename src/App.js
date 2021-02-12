2383e392faf3307d13e7b3aa3d41c7799c35c577
unknown <markdoma10@gmail.com>
2021-02-12 16:32:01 +0800

    add custombutton for form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import HomePage from "./Pages/Homepage/Homepage.component";
 import ShopPage from "./Pages/Shop/shop.component";
 import Header from "./Components/header/header.component";
 
+import SignInAndSignUpPage from "./Pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
+
 function App() {
   return (
     <div>
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/Components/custom-button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './custom-button.styles.scss'
+
+const CustomButton =() => (
+
+)
+
+Export default CustomButton;
