36d5177c4e9f86a0378fd221a8ef60980af35372
unknown <markdoma10@gmail.com>
2021-03-12 16:10:30 +0800

    add checkout page, checkou-item component and add dispatch to cart-dropdown

--- src/Pages/checkout/checkout.component.jsx
+++ src/Pages/checkout/checkout.component.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCartItems } from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../Components/checkout-item/checkout-item.component";
+
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = () => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
@@ -25,7 +30,20 @@ const CheckoutPage = () => (
         <span>REMOVE</span>
       </div>
     </div>
+
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+
+    <div className="total">
+      <span>TOTAL: ${total}</span>
+    </div>
   </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
